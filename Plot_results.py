acb52f207d391311e368fa03b8d44d937e1a3490
DongJo Kim <dong.jo.kim@cern.ch>
2023-01-08 17:14:39 +0200

    working ploting

--- Plot_results.py
+++ Plot_results.py
@@ -6,22 +6,69 @@ import matplotlib as mpl
 import keras
 # save np.load
 np_load_old = np.load
+cmap = plt.get_cmap('gray_r')
 
 # modify the default parameters of np.load
 np.load = lambda *a,**k: np_load_old(*a, allow_pickle=True, **k)
+def pad_image(image, max_size = (16,22)):
+    """
+    Simply pad an image with zeros up to max_size.
+    """
+    size = np.shape(image)
+    px, py = (max_size[0]-size[0]), (max_size[1]-size[1])
+    image = np.pad(image, (list(map(int,((np.floor(px/2.), np.ceil(px/2.))))), list(map(int,(np.floor(py/2.), np.ceil(py/2.))))), 'constant')
+    return image
 
-model_dir='trained_models/'
+def normalize(histo, multi=255):
+    """
+    Normalize picture in [0,multi] range, with integer steps. E.g. multi=255 for 256 steps.
+    """
+    return (histo/np.max(histo)*multi).astype(int)
 
+############ Load data 
+outdir = 'images_out/'
+data0 = np.load(outdir+'qcd_leading_jet.npz')['arr_0']
+data1 = np.load(outdir+'tt_leading_jet.npz')['arr_0']
+
+print('We have {} QCD jets and {} top jets'.format(len(data0), len(data1)))
+
+x_data = np.concatenate((data0, data1))
+# pad and normalize images
+x_data = list(map(pad_image, x_data))
+x_data = list(map(normalize, x_data))
+
+y_data = np.array([0]*len(data0)+[1]*len(data1))
+
+np.random.seed(0) # for reproducibility
+x_data, y_data = np.random.permutation(np.array([x_data, y_data]).T).T
+
+# the data coming out of previous commands is a list of 2D arrays. We want a 3D np array (n_events, xpixels, ypixels)
+x_data = np.stack(x_data)
+
+print(x_data.shape, y_data.shape)
+
+# reshape for tensorflow: x_data.shape + (1,) = shortcut for (x_data.shape[0], 16, 22, 1)
+x_data = x_data.reshape(x_data.shape + (1,)).astype('float32')
+x_data /= 255.
+
+y_data = keras.utils.to_categorical(y_data, 2)
+
+print(x_data.shape, y_data.shape)
+
+n_train = 50000
+(x_train, x_test) = x_data[:n_train], x_data[n_train:]
+(y_train, y_test) = y_data[:n_train], y_data[n_train:]
+print('We will train+validate on {0} images, leaving {1} for cross-validation'.format(n_train,len(x_data)-n_train))
+
+print(x_test.shape)
+
+model_dir='trained_models_low_pt/'
 history_logi, history_mlp, history_cnn = np.load(model_dir+'training_histories.npz')['arr_0']
 model0 = keras.models.load_model(model_dir+'logi.h5')
 model1 = keras.models.load_model(model_dir+'mlp.h5')
 model_cnn = keras.models.load_model(model_dir+'cnn.h5')
-print(history_logi)
-outdir = 'images_out/'
-data = np.load(outdir+'allimages.npz')['arr_0']
-x_test = data[0]
-y_test = data[0]
-print(x_test.shape)
+#print(history_logi)
+
 predictions0 = model0.predict(x_test)
 predictions1 = model1.predict(x_test)
 predictions_cnn = model_cnn.predict(x_test)
@@ -51,17 +98,17 @@ logscale = dict(norm=mpl.colors.LogNorm()) if logscale else {}
 
 fig, axes = plt.subplots(1,5,figsize=(20,2.5))
 for i in range(2):
-    im = axes[i].imshow(x_test[predictions_cnn.argmin(axis=0)[i],:,:,0], vmax=1, cmap=cmap, **logscale)
-    plt.colorbar(im, ax=axes[i])
-    axes[i].set(title='most {}-like jet'.format(['top','qcd'][i]))
-axes[2].imshow(x_test[abs(predictions_cnn-0.5).argmin(axis=0)[0],:,:,0], vmax=1, cmap=cmap, **logscale)
+	im = axes[i].imshow(x_test[predictions_cnn.argmin(axis=0)[i],:,:,0], cmap=cmap, **logscale)
+	plt.colorbar(im, ax=axes[i])
+	axes[i].set(title='most {}-like jet'.format(['top','qcd'][i]))
+axes[2].imshow(x_test[abs(predictions_cnn-0.5).argmin(axis=0)[0],:,:,0], cmap=cmap, **logscale)
 plt.colorbar(im, ax=axes[2])
 axes[2].set(title='most uncertain jet');
 
 for iax, i in enumerate((predictions_cnn - y_test).argmin(axis=0)):
-    im = axes[iax+3].imshow(x_test[i,:,:,0], vmax=1, cmap=cmap, **logscale)
-    plt.colorbar(im, ax=axes[iax+3])
-    axes[iax+3].set_title('Output: {}\nLabel = {}    --FAIL--'.format(predictions_cnn[i], y_test[i]))
+	im = axes[iax+3].imshow(x_test[i,:,:,0], cmap=cmap, **logscale)
+	plt.colorbar(im, ax=axes[iax+3])
+	axes[iax+3].set_title('Output: {}\nLabel = {}    --FAIL--'.format(predictions_cnn[i], y_test[i]))
 
 fig.tight_layout()
 fig.savefig('figs/cnn_jet_sample.png', dpi=150)
